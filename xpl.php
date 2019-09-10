e95c1f61329e724a5e550f5eaf6a3d2816980e43
Arnaud BODEL <contact@osw3.net>
2019-09-11 00:54:09 +0200

    add xtrim

--- xpl.php
+++ xpl.php
@@ -4,10 +4,10 @@ define('XPL_LIBRARY', __DIR__.'/lib/');
 
 $library = scandir(XPL_LIBRARY);
 
-foreach ($library as $file) 
+foreach ($library as $feature) 
 {
-    if (preg_match("/\.php$/", $file)) 
+    if (preg_match("/\.php$/", $feature)) 
     {
-        require_once XPL_LIBRARY.$file;
+        require_once XPL_LIBRARY.$feature."/".$feature.".php";
     }
 }
